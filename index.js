1bc20abbeae1a9b8e00ca77448c06db84d4aaa06
Scarlett Eller <rq6715@gmail.com>
2021-12-02 16:48:51 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const person = [`Guadalupe`, `Ollie`, `Aki`]
+
+function writeCards() {
+    let array = [];
+    for (let i = 0; i < person.length; i++) {
+        let message = (`Thank you, ${person[i]}, for the wonderful surprise gift!`)
+        array.push(message);
+    }
+    return array;
+}
+
+writeCards(person);
+
+function countDown() {
+    let count = 11
+    while (count > 0) {
+        count -= 1;
+    console.log(count);
+    }
+}
+
+countDown();
+
+
